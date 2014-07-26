30d6a17add7c3f544d95f3aaeb581ffd87ca4be3
Eric Yu <chunkaiplus@gmail.com>
2014-07-26 14:56:05 +0800

    Add basic layout for web index

---
+++ bgradar/assets/js/controller.js
@@ -0,0 +1,8 @@
+angular.module('bgradarApp.controllers', [])
+  .controller('WallController', ['$scope',
+  	 function($scope) {
+  	 	var init = function(){
+			console.log("Hello world");
+		}
+		init();
+  	}])
---
+++ bgradar/static/assets/js/controller.js
@@ -0,0 +1,1 @@
+angular.module("bgradarApp.controllers",[]).controller("WallController",["$scope",function(){var init=function(){console.log("Hello world")};init()}]);
