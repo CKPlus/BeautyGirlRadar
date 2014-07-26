141f70880dfdb881f0f23a3ad0f26fbe1d5d6222
Wes <takachi0322@gmail.com>
2014-07-27 06:41:53 +0800

    googleapi

---
+++ bgradar/api/data/google_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import json
+import urllib3
+
+
+http = urllib3.PoolManager(10)
+GOOGLE_GEOCODE_API_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+GOOGLE_API_KEY = 'AIzaSyDdJ6sYjPSXJBMu3j1CjWoxX0rIVrNUEgQ'
+
+
+def get_address_by_lnglat(lng, lat):
+    address = ''
+    google_geocode_url = GOOGLE_GEOCODE_API_URL + "?sensor=true&latlng={0},{1}".format(lat, lng)
+
+    google_geocode_url = google_geocode_url + '&language=zh-TW'
+
+    resp = http.urlopen('GET', google_geocode_url)
+
+    google_geocode = json.loads(resp.data)
+
+    if 'results' not in google_geocode:
+        return address
+
+    if len(google_geocode['results']) == 0:
+        return address
+
+    address = google_geocode['results'][0].get('formatted_address', '')
+
+    return address
+
+
+if __name__ == '__main__':
+    print get_address_by_lnglat(121.508272, 25.0421569)
